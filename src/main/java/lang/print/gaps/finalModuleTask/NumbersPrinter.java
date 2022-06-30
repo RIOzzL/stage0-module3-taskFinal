80c80fa88cc4793460f6ef772095e3d457923752
Roman Gulevich <R.I.Gulevich@gmail.com>
2022-06-30 22:35:06 +0300

    Module3 - FinalTask

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        System.out.println(number > 0);
     }
 }
